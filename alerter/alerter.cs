8f97befedce2b8ff4f8710f96b45f1cad809e8a5
sagar-sagi <31595868+sagar-sagi@users.noreply.github.com>
2022-11-29 21:25:09 +0530

    Added assert to check AlertFailureCount

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -19,6 +19,7 @@ namespace AlerterSpace {
                 // However, this code doesn't count failures!
                 // Add a test below to catch this bug. Alter the stub above, if needed.
                 alertFailureCount += 0;
+                Debug.Assert(alertFailureCount > 0);
             }
         }
         static void Main(string[] args) {
