6adbae20ac266d04e49e11d4ea9954a7f076fe53
sagar-sagi <31595868+sagar-sagi@users.noreply.github.com>
2022-11-29 21:00:35 +0530

    Added an assert to verify color code

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -3,20 +3,29 @@ using System.Diagnostics;
 
 namespace MisalignedSpace {
     class Misaligned {
-        static int printColorMap() {
+        static string printColorMap(int number) {
+            string result = "";
             string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
             string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
             int i = 0, j = 0;
             for(i = 0; i < 5; i++) {
                 for(j = 0; j < 5; j++) {
+                    if(number == (i * 5 + j))
+                    {
+                        result = String.Format("{0} | {1}", majorColors[i], minorColors[i]);
+                    }
                     Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
                 }
             }
-            return i * j;
+            return result;
         }
         static void Main(string[] args) {
-            int result = printColorMap();
-            Debug.Assert(result == 25);
+            string result = printColorMap(4);
+            Debug.Assert(result == "White | Slate");
+            
+            result = printColorMap(22);
+            Debug.Assert(result == "Violet | Blue");
+            
             Console.WriteLine("All is well (maybe!)");
         }
     }
