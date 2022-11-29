eab83fdcc35d696cdb738d0ad01e77d842bc8d1d
sagar-sagi <31595868+sagar-sagi@users.noreply.github.com>
2022-11-29 22:44:21 +0530

    Edited the stub code to return non-ok values too

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -9,7 +9,8 @@ namespace AlerterSpace {
             // Return 200 for ok
             // Return 500 for not-ok
             // stub always succeeds and returns 200
-            return 200;
+            if(celcius < 200) return 200;
+            else return 500;
         }
         static void alertInCelcius(float farenheit) {
             float celcius = (farenheit - 32) * 5 / 9;
