e47e58b68487cf21e1804af0cd2b6e23d7129310
sagar-sagi <31595868+sagar-sagi@users.noreply.github.com>
2022-12-07 23:10:01 +0530

    Bug Fix: alertFailureCount is incremented whenever alert is trigerred

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -20,7 +20,7 @@ namespace AlerterSpace {
                 // let us keep a count of failures to report
                 // However, this code doesn't count failures!
                 // Add a test below to catch this bug. Alter the stub above, if needed.
-                alertFailureCount += 0;
+                alertFailureCount ++;
                 Debug.Assert(alertFailureCount > 0);
             }
         }
